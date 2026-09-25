32e7b770a162078ce1412e9cf97e833f8f7981be
Aditya Pratap Singh <adpratap24@gmail.com>
2026-09-25 15:42:53 +0530

    reducers added

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,20 @@
- import { createSlice } from '@reduxjs/toolkit'
+ import { createSlice, nanoid } from '@reduxjs/toolkit'
 
   const initialState = {
         todos: [{id: "abc", task:"demo-task", isDone: false}],
     };
 
-    export const todoSlice = createSlice({})
+    export const todoSlice = createSlice({
+        name: "todo",
+        initialState,
+        reducers: { //state, action
+            addTodo: (state, action) => { // state variable ki baathorhi hai  or action mtl jo event occur hora hai
+                const newTodo = {
+                    id: nanoid(),
+                    task: action.payload, // is se humare pas task aajayega
+                    isDone: false,
+                }   
+                state.todos.push(newTodo); // state.todos mtlb state ke andr todos ko access kiya
+            }
+        }
+    })
