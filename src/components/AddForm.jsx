e45d3880e606acde8a3b0f4f3468d77071d50492
Aditya Pratap Singh <adpratap24@gmail.com>
2026-09-27 00:28:10 +0530

    delete initiated with nano id

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -2,22 +2,25 @@ import { useState } from "react"
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
-export default function AddForm(){
-    const [task,setTask] = useState("");
-    const dispatch = useDispatch();
+export default function AddForm() {
+  const [task, setTask] = useState("");
+  const dispatch = useDispatch();
 
-    const submitHandler = (evt) =>{
-        evt.preventDefault();
-        console.log(task);
-        dispatch(addTodo(task))
-    }
-    
-    return(
-        <>
-            <form onSubmit={submitHandler}>
-                <input type="text" onChange={(e) => setTask(e.target.value)}></input>
-                <button>Add Task</button>
-            </form>
-        </>
-    )
+  const submitHandler = (evt) => {
+    evt.preventDefault();
+    if (!task.trim()) return;
+    dispatch(addTodo(task.trim()));
+    setTask("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,28 @@
 import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
 
-export default function Todo(){
-        const todos = useSelector((state) => state.todos);
-        console.log(todos);
-        return(
-        <div>
-            <h3>todo</h3>
-            <ul>
-                {todos.map((todo) =>{
-                    <li key={todo.id}> {todo.task} </li>
-                })}
-            </ul>
-        </div>
-    )
+export default function Todo() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+    const dispatch = useDispatch();
+
+  const clickHandler = (id) =>{
+    console.log("delete",id );
+    dispatch(deleteTodo(id));
+  }
+
+  return (
+    <div>
+      <h3>todo</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            <button type="button" onClick={ () => clickHandler(todo.id) }>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
